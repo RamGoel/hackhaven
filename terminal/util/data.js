4497e9b36599a9d01d1e9ce72a5860a14352e229
Ram Goel <rgoel766@gmail.com>
2023-06-11 13:26:18 +0530

    added static code

---
+++ terminal/util/data.js
@@ -0,0 +1,37 @@
+export const data = {
+    "gdsc": `
+    Google Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a GDSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.
+    `,
+    "abesec": `
+    ABES Engineering College is one of the top engineering Colleges of UPTU in Delhi,NCR. AICTE Approved and AKTU Affiliated college.
+    `,
+    "about": `"Hack Haven: Where Innovation Finds its Home!" is a hackathon that brings together the brightest minds from colleges and universities  to create a solution for real world problems. The students will  have the chance to collaborate with like-minded individuals, share knowledge and learn from each other. Throughout the hackathon, you’ll have access to mentors and industry experts, providing invaluable mentorship and guidance.
+    This hackathon isn’t just about the competition, it’s about building lasting connections and forging a network of talented individuals.`,
+    "prizes": "Will be revealed soon",
+    "sponsors": "Will be revealed soon",
+    "team": "Will Reveal Soon",
+    "venue": "TL01-TL02 Kalpana Chawla Block, ABES Engineering College, Campus 1, 19th KM Stone, NH-24, Ghaziabad-201009, U.P.",
+    "date": `8 July, 2023 9:00 AM`,
+    "register":`Will be opening on 15th July 2023`,
+    "schedule": `
+        12 July, 2023:              Announcement
+        15 July, 2023:              Registrations
+        16 July, 2023:              Call for sponsors, community partners and speakers
+        24 July, 2023:              Online round
+        30 July, 2023:              End of Online round
+        3 - 7 July, 2023:           Pre-events and webinars
+        8 July, 2023:               12 hr Hackathon (offline round)
+    `,
+    "help": `
+        gdsc:             Know about GDSC
+        abesec:           Know about ABESEC
+        about:            Know about Hackathon
+        register:         Register for the hackathon
+        prizes:           Know about prizes
+        sponsors:         Know about Sponsors
+        venue:            Know about Venue
+        date:             Know Hackathon Dates & Time
+        schedule:         Know about Schedule of Hackathon
+        team:             Know about our Team
+    `
+}
---
+++ terminal/util/power.js
@@ -0,0 +1,35 @@
+import { click } from "../sound/index.js";
+import { boot } from "./screens.js";
+import { stopSpeaking } from "./speak.js";
+import pause from "./pause.js";
+
+/** Turn on the terminal */
+async function on() {
+	click();
+	await power();
+	boot();
+	alert("njnj")
+}
+
+/** Turn off the terminal */
+function off() {
+	click();
+	stopSpeaking();
+	power(false);
+}
+
+async function power(on = true) {
+	// @FIXME use a single class on the #monitor to detect on/off
+	document.querySelector("#slider").classList.toggle("on", on);
+	document.querySelector("#switch").checked = !on;
+	await pause(0.1);
+
+	document.getElementById("monitor").classList.toggle("turn-off", !on);
+	document.getElementById("monitor").classList.toggle("off", !on);
+
+	boot();
+
+	return;
+}
+
+export { power, on, off };
